925bf84049f6a2fad097d8a7ac63c18f1c62ea0c
garylvov <67614381+garylvov@users.noreply.github.com>
2021-03-18 11:08:42 -0400

    Create sparky.ino

---
+++ arduino/sparky.ino
@@ -0,0 +1,120 @@
+#include <ros.h>
+#include <sensor_msgs/JointState.h>
+#include <trajectory_msgs/JointTrajectory.h>
+#include <trajectory_msgs/JointTrajectoryPoint.h>
+#include <sensor_msgs/Imu.h>
+
+#include <Dynamixel2Arduino.h>
+//#include <Wire.h>
+//#include <Adafruit_Sensor.h>
+//#include <Adafruit_BNO055.h>
+
+#define DXL_SERIAL Serial1
+#define DEBUG_SERIAL Serial
+#define MOVING_SPEED_ADDR 32
+#define MOVING_SPEED_ADDR_LEN 2
+#define TIMEOUT 10
+
+uint16_t movingSpeed = 400;
+const uint8_t DXL_DIR_PIN = 28;
+const float DXL_PROTOCOL_VERSION = 1.0;
+
+char *joint_names[12] = {"lf_hip_to_base_link", "lf_upper_leg_to_hip", "lf_lower_leg_to_upper_leg",
+                         "rf_hip_to_base_link", "rf_upper_leg_to_hip", "rf_lower_leg_to_upper_leg",
+                         "lh_hip_to_base_link", "lh_upper_leg_to_hip", "lh_lower_leg_to_upper_leg",
+                         "rh_hip_to_base_link", "rh_upper_leg_to_hip", "rh_lower_leg_to_upper_leg"
+                        };
+
+const float offset[12] = {1.0472, 1.0472, 4.18879,   //radian offset from 0 servo position to 0 joint position on URDF
+                          4.18879, 4.18879, 1.0472,
+                          4.18879, 1.0472, 4.18879,
+                          1.0472, 4.18879, 1.0472
+                         };
+
+const float orientation[12] = {1,  1,  1,
+                               1, -1, -1,
+                               1,  1,  1,
+                               1, -1, -1
+                              };
+
+float pos[12] = {0, 0, 0,
+                 0, 0, 0,
+                 0, 0, 0,
+                 0, 0, 0
+                };
+
+float desired[12];
+
+ros::NodeHandle nh;
+sensor_msgs::JointState joint_states;
+trajectory_msgs::JointTrajectoryPoint points;
+
+void jointtraj_callback(const trajectory_msgs::JointTrajectory& msg) {
+  points = msg.points[0];
+  for (int x = 0; x < 12; x++) {
+    desired[x] = points.positions[x];
+  }
+}
+
+
+//sensor_msgs::Imu imu_msg;
+ros::Subscriber <trajectory_msgs::JointTrajectory> jointtraj_sub("/joint_group_position_controller/command", &jointtraj_callback);
+ros::Publisher joint_states_pub("joint_states", &joint_states);
+//ros::Publisher imu_pub("imu", &imu_msg);
+
+Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
+
+//Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+void setup() {
+  nh.initNode();
+  nh.advertise(joint_states_pub);
+  //nh.advertise(imu_pub);
+  nh.subscribe(jointtraj_sub);
+  joint_states.name_length = 12;
+  joint_states.position_length = 12;
+  joint_states.name = joint_names;
+
+  DEBUG_SERIAL.begin(115200);
+  dxl.begin(1000000);
+  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+
+  for (int id = 1; id <= 12; id++) {
+    dxl.torqueOff(id);
+    dxl.setOperatingMode(id, OP_POSITION);
+    dxl.torqueOn(id);
+  }
+  //bno.setExtCrystalUse(true);
+}
+
+void loop() {
+  //read_imu();
+  read_servos();
+  write_servos();
+  nh.spinOnce();
+}
+/*
+void read_imu() {
+  imu::Quaternion quat = bno.getQuat();
+  imu_msg.orientation.w = quat.w();
+  imu_msg.orientation.x = quat.x();
+  imu_msg.orientation.y = quat.y();
+  imu_msg.orientation.z = quat.z();
+  imu_pub.publish(&imu_msg);
+}
+*/
+void read_servos() {
+  for (int i = 0; i < 12; i++) {
+    pos[i] = ((((dxl.getPresentPosition((i + 1), UNIT_DEGREE)) * (3.14159 / 180)) - offset[i]) * orientation[i]);
+  }
+  joint_states.position = pos;
+  joint_states.header.stamp = nh.now();
+  joint_states_pub.publish(&joint_states);
+}
+
+void write_servos() {
+  for (int i = 0; i < 12; i++) {
+    dxl.write(i+1, MOVING_SPEED_ADDR, (uint8_t*)&movingSpeed, MOVING_SPEED_ADDR_LEN, TIMEOUT);
+    dxl.setGoalPosition((i + 1), ((((desired[i] * orientation[i]) + offset[i]) * 180) / 3.14159), UNIT_DEGREE);
+  }
+}
